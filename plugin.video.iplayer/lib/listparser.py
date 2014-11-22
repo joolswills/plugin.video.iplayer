1abe9e66121837448c9e630da706cb8f4a98be21
Jools Wills <buzz@exotica.org.uk>
2014-11-22 03:36:19 +0000

    updated: replace the horrible regexp based listparser with a simpler cleaner solution using elementtree

--- plugin.video.iplayer/lib/listparser.py
+++ plugin.video.iplayer/lib/listparser.py
@@ -3,12 +3,7 @@
 #
 
 import re
-
-def xmlunescape(data):
-    data = data.replace('&amp;', '&')
-    data = data.replace('&gt;', '>')
-    data = data.replace('&lt;', '<')
-    return data
+from xml.etree import ElementTree as ET
 
 class listentry(object):
      def __init__(self, title=None, id=None, updated=None, summary=None, categories=None, series=None, episode=None, thumbnail=None):
@@ -25,62 +20,37 @@ class listentries(object):
      def __init__(self):
          self.entries = []
 
-def parse(xmlSource):
-    try:
-        encoding = re.findall( "<\?xml version=\"[^\"]*\" encoding=\"([^\"]*)\"\?>", xmlSource )[ 0 ]
-    except:
-        return None
-
-    elist=listentries()
-    # gather all list entries
-    entriesSrc = re.findall("<episode>(.*?)</episode>", xmlSource, re.DOTALL)
+def parse(xml):
+    
     datematch = re.compile(':\s+([0-9]+)/([0-9]+)/([0-9]{4})')
 
-    re_title = re.compile("<complete_title>(.*?)</complete_title>", re.DOTALL)
-    re_id = re.compile("<id>(.*?)</id>", re.DOTALL)
-    re_updated = re.compile("<updated>(.*?)</updated>", re.DOTALL)
-    re_summary = re.compile("<synopsis>(.*?)</synopsis>", re.DOTALL)
-    re_categories = re.compile("<category.*?<short_name>(.*?)</short_name>.*?</category>", re.DOTALL)
-    re_thumbnail = re.compile("<my_image_base_url>(.*?)</my_image_base_url>", re.DOTALL)
-    re_series = re.compile("<link rel=\"related\" href=\".*microsite.*title=\"(.*?)\" />", re.DOTALL)
+    # remove namespace
+    xml = re.sub(' xmlns="[^"]+"', '', xml, count=1)
 
-    episode_exprs = [ re.compile("<link rel=\"self\" .*title=\".*pisode *([0-9]+)", re.DOTALL),
-                      re.compile("<link rel=\"self\" .*title=\"([0-9]+)\.", re.DOTALL) ]
+    root = ET.fromstring(xml)
 
-    # enumerate thru the element list and gather info
-    for entrySrc in entriesSrc:
-        entry={}
-        title   = re_title.findall(entrySrc)[0]
-        id      = re_id.findall(entrySrc)[-1]
-        updated = re_updated.findall(entrySrc)[0]
-        summary = re_summary.findall(entrySrc)[0]
-        categories = re_categories.findall(entrySrc)
-        thumbnail = re_thumbnail.findall(entrySrc)[0]
-        thumbnail += id + "_640_360.jpg"
+    elist = listentries()
+    for entry in root.getiterator('episode'):
+        title = entry.find('complete_title').text
+        id = entry.find('id').text
+        updated = entry.find('updated').text
+        summary = entry.find('synopsis').text
+        thumbnail = entry.find('my_image_base_url').text + id + "_640_360.jpg"
 
-
-
-        series = re_series.findall(entrySrc)
-        if len(series):
-            series = series[0]
-        else:
-            series = title
-
-        episode = None
-        for ex in episode_exprs:
-            e = ex.findall(entrySrc)
-            if len(e):
-                episode = "%s:%02d" % (series, int(e[0]))
-                break;
-        episode = episode or title
+        series = entry.find('toplevel_container_title').text
+        episode = entry.find('position').text
+        if episode is not None:
+            episode = int(episode)
 
         match = datematch.search(title)
         if match:
             # if the title contains a data at the end use that as the updated date YYYY-MM-DD
-            updated = "%s-%s-%s" % ( match.group(3), match.group(2), match.group(1)  )
+            updated = "%s-%s-%s" % ( match.group(3), match.group(2), match.group(1) )
 
-        e_categories=[]
-        for c in categories: e_categories.append(xmlunescape(c))
-        elist.entries.append(listentry(xmlunescape(title), xmlunescape(id), xmlunescape(updated), xmlunescape(summary), e_categories, series, episode, xmlunescape(thumbnail)))
+        e_categories = []
+        for category in entry.find('categories').findall('category'):
+            e_categories.append(category.find('short_name').text)
+
+        elist.entries.append(listentry(title, id, updated, summary, e_categories, series, episode, thumbnail))
 
     return elist
