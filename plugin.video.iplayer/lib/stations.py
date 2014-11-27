80b0f6111fcb2d6de7a5c98bea0b5ac7f3a419bd
Jools Wills <buzz@exotica.org.uk>
2014-11-27 16:09:30 +0000

    changed: re-added s4c - seems to have programmes again

--- plugin.video.iplayer/lib/stations.py
+++ plugin.video.iplayer/lib/stations.py
@@ -14,6 +14,7 @@ channels_tv_list = [
     ('bbc_news24', 'BBC News Channel'),
     ('bbc_parliament', 'BBC Parliament'),
     ('bbc_alba', 'BBC Alba'),
+    ('s4cpbs', 'S4C'),
     ('bbc_sport', 'BBC Sport'),
 ]
 
